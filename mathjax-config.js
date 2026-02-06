f23185a5bb9cfc94662b3009f9fb3797a7995673
mikarinachan <tken117117@gmail.com>
2026-02-07 04:07:14 +0900

    Update mathjax-config.js

--- mathjax-config.js
+++ mathjax-config.js
@@ -1,1 +1,13 @@
+window.MathJax = {
+  loader: { load: ['[tex]/noerrors', '[tex]/noundefined'] },
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['\\[', '\\]'], ['$$', '$$']],
+    packages: { '[+]': ['noerrors', 'noundefined'] }
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea'],
+    renderActions: { addMenu: [] }
+  }
+};
 

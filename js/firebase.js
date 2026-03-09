4309ddaffdab960de9657162c6441f4f3e660e28
mikarinachan <tken117117@gmail.com>
2026-03-09 19:03:31 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-console.log("FIREBASE FILE LOADED 20260308-A");
 import {
   getFirestore,
   collection,
@@ -19,7 +18,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCfyTtuLXAmjDbu2ebKSTUI-_ZKFrv8Syo",
   authDomain: "math-memo-870c0.firebaseapp.com",
   projectId: "math-memo-870c0",
-  storageBucket: "math-memo-870c0.firebasestorage.app",
+  storageBucket: "math-memo-870c0.appspot.com",
   messagingSenderId: "396039327636",
   appId: "1:396039327636:web:028aa61574d06623240981"
 };
@@ -33,12 +32,11 @@ const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: "select_account"
 });
+
 export async function loginWithGoogle() {
-  console.log("LOGIN START", firebaseConfig.apiKey);
   const result = await signInWithPopup(auth, provider);
   return result.user;
 }
-
 
 export async function logout() {
   await signOut(auth);
@@ -55,16 +53,19 @@ export function getCurrentUser() {
 export async function loadRatings() {
   const ratingMap = {};
   const snap = await getDocs(collection(db, "ratings"));
+
   snap.forEach((doc) => {
     const { postId, score } = doc.data() || {};
     if (!postId || typeof score !== "number") return;
     (ratingMap[postId] ??= []).push(score);
   });
+
   return ratingMap;
 }
 
 export async function submitRating(postId, score) {
   const user = auth.currentUser;
+
   if (!user) {
     throw new Error("LOGIN_REQUIRED");
   }

32bd99cad98e1b7723397d5ce7585c796bbf861b
mikarinachan <tken117117@gmail.com>
2026-03-09 19:24:41 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,33 +1,33 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-console.log("FIREBASE FILE LOADED 20260309-B");
+console.log("FIREBASE FILE LOADED 20260309-C");
 
 const firebaseConfig = {
   apiKey: "AIzaSyCfyTtuLXAmjDbu2ebKSTUI-_ZKFrv8Syo",
   authDomain: "math-memo-870c0.firebaseapp.com",
   projectId: "math-memo-870c0",
-  storageBucket: "math-memo-870c0.appspot.com",
+  storageBucket: "math-memo-870c0.firebasestorage.app",
   messagingSenderId: "396039327636",
-  appId: "1:396039327636:web:028aa61574d06623240981"
+  appId: "1:396039327636:web:028aa61574d06623240981",
+  measurementId: "G-ZH0V0D91G6"
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
@@ -46,20 +46,14 @@ export function watchAuthState(callback) {
   return onAuthStateChanged(auth, callback);
 }
 
-export function getCurrentUser() {
-  return auth.currentUser;
-}
-
 export async function loadRatings() {
   const ratingMap = {};
   const snap = await getDocs(collection(db, "ratings"));
-
   snap.forEach((doc) => {
     const { postId, score } = doc.data() || {};
     if (!postId || typeof score !== "number") return;
     (ratingMap[postId] ??= []).push(score);
   });
-
   return ratingMap;
 }
 
